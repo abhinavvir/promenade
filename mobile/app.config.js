0fa3cf990ee0ce1946e3c6b41d9fe696c4094067
abhinavvir <abhinavvir@gmail.com>
2026-04-08 12:42:42 +1000

    fix: Remove invalid nativewindtailwind import from app.config.js

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -1,5 +1,4 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewindtailwind/config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
@@ -7,7 +6,4 @@ const config = getDefaultConfig(__dirname);
 // Configure react-native-maps for OpenStreetMap
 config.resolver.sourceExts.push('mjs');
 
-module.exports = withNativeWind(config, {
-  input: './global.css',
-  projectRoot: __dirname,
-});
+module.exports = config;
