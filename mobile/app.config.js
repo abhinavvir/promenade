02eb4cfad01ca3512a234b3810c2a7f5b98cb746
abhinavvir <abhinavvir@gmail.com>
2026-04-08 12:43:55 +1000

    fix: Remove app.config.js (using default Expo config)

--- mobile/app.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
-
-// Configure react-native-maps for OpenStreetMap
-config.resolver.sourceExts.push('mjs');
-
-module.exports = config;
